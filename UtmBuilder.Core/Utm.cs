c919791f5281f96782e3323b90cc8f685a53e86c
RichardKT88 <kendy_t88@hotmail.com>
2023-02-13 18:57:15 -0300

    Add project files.

---
+++ UtmBuilder.Core/Utm.cs
@@ -0,0 +1,24 @@
+﻿using UtmBuilder.Core.ValueObjects;
+
+namespace UtmBuilder.Core;
+
+public class Utm
+{
+    public Utm(Url url, Campaing campaing)
+    {
+        Url = url;
+        Campaing = campaing;
+    }
+
+    //null! ==> null not (não pode ser nulo, mas não tem um objeto ainda)
+    /// <summary>
+    /// URL (Website Link)
+    /// </summary>
+    public Url Url { get; }
+
+    /// <summary>
+    /// Campaign Details
+    /// </summary>
+    public Campaing Campaing { get; }
+
+}
